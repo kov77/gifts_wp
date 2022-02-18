590b5053dfd93cbf7142ba5e52f6d3fb49000ffc
Maksym_Koval <maksim6167@gmail.com>
2022-02-18 14:44:32 -0400

    Завершена секция 'Развивающие игрушки'

--- index.php
+++ index.php
@@ -159,64 +159,78 @@
                 <h2 class="subtitle">Мягкие игрушки</h2>
                 <div class="toys__wrapper">
                     <?php
-                            // параметры по умолчанию
-                            $my_posts = get_posts( array(
-                                'numberposts' => -1,
-                                'category_name'    => 'soft_toys ',
-                                'orderby'     => 'date',
-                                'order'       => 'ASC',
-                                'post_type'   => 'post',
-                                'suppress_filters' => true,
-                            ) );
+                        // параметры по умолчанию
+                        $my_posts = get_posts( array(
+                            'numberposts' => -1,
+                            'category_name'    => 'soft_toys ',
+                            'orderby'     => 'date',
+                            'order'       => 'ASC',
+                            'post_type'   => 'post',
+                            'suppress_filters' => true,
+                        ) );
 
-                            foreach( $my_posts as $post ){
-                                setup_postdata( $post );
-                        ?>
-                            <div class="toys__item" style="background-image: url(<?php 
-                            if(has_post_thumbnail()) {
-                                the_post_thumbnail_url();
-                            } else {
-                                echo get_template_directory_uri() . '/assets/img/not-found.jpg';
-                            }
-                             ?>)">
-                                <div class="toys__item-info">
-                                    <div class="toys__item-title"><?php the_title() ?></div>
-                                    <div class="toys__item-descr">
-                                        <?php the_field('toys_descr') ?>
-                                    </div>
-                                    <div class="minibutton toys__trigger">Подробнее</div>
+                        foreach( $my_posts as $post ){
+                            setup_postdata( $post );
+                    ?>
+                        <div class="toys__item" style="background-image: url(<?php 
+                        if(has_post_thumbnail()) {
+                            the_post_thumbnail_url();
+                        } else {
+                            echo get_template_directory_uri() . '/assets/img/not-found.jpg';
+                        }
+                            ?>)">
+                            <div class="toys__item-info">
+                                <div class="toys__item-title"><?php the_title() ?></div>
+                                <div class="toys__item-descr">
+                                    <?php the_field('toys_descr') ?>
                                 </div>
+                                <div class="minibutton toys__trigger">Подробнее</div>
                             </div>
-                        <?php
-                            }
+                        </div>
+                    <?php
+                        }
 
-                            wp_reset_postdata(); // сброс
-                        ?>
-                    </div>
+                        wp_reset_postdata(); // сброс
+                    ?>
+                </div>
 
 
                 <h2 class="subtitle">Развивающие игрушки</h2>
                 <div class="toys__wrapper">
+                <?php
+                        // параметры по умолчанию
+                        $my_posts = get_posts( array(
+                            'numberposts' => -1,
+                            'category_name'    => 'edu_toys ',
+                            'orderby'     => 'date',
+                            'order'       => 'ASC',
+                            'post_type'   => 'post',
+                            'suppress_filters' => true,
+                        ) );
 
-                    <div class="toys__item" style="background-image: url(<?php echo bloginfo('template_url'); ?>/assets/img/toy_7.jpg)">
-                        <div class="toys__item-info">
-                            <div class="toys__item-title">Воздушный змей</div>
-                            <div class="toys__item-descr">
-                                Кто в детстве не хотел научиться летать? А змей поможет поймать ветер и унести все заботы далеко-далеко...
+                        foreach( $my_posts as $post ){
+                            setup_postdata( $post );
+                    ?>
+                        <div class="toys__item" style="background-image: url(<?php 
+                        if(has_post_thumbnail()) {
+                            the_post_thumbnail_url();
+                        } else {
+                            echo get_template_directory_uri() . '/assets/img/not-found.jpg';
+                        }
+                            ?>)">
+                            <div class="toys__item-info">
+                                <div class="toys__item-title"><?php the_title() ?></div>
+                                <div class="toys__item-descr">
+                                    <?php the_field('toys_descr') ?>
+                                </div>
+                                <div class="minibutton toys__trigger">Подробнее</div>
                             </div>
-                            <div class="minibutton toys__trigger">Подробнее</div>
                         </div>
-                    </div>
+                    <?php
+                        }
 
-                    <div class="toys__item" style="background-image: url(<?php echo bloginfo('template_url'); ?>/assets/img/toy_8.jpg)">
-                        <div class="toys__item-info">
-                            <div class="toys__item-title">Музыкальные</div>
-                            <div class="toys__item-descr">
-                                Попробуйте заинтересовать ребенка музыкой! Может в нем таится будущий Джаред Лето!
-                            </div>
-                            <div class="minibutton toys__trigger">Подробнее</div>
-                        </div>
-                    </div>
+                        wp_reset_postdata(); // сброс
+                    ?>
 
                 </div>
                 <div class="row">
